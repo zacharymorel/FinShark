72bd2a080334a945ee6e10dc87074b6a78ec62f0
zacharymorel <zacharyjmorel@gmail.com>
2024-03-28 14:03:20 -0400

    Added Stock and Comment models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,14 @@
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        // The "?"is optional. Not required for FK to be present.
+        public int? StockId { get; set; } // FK
+        public Stock? Stock { get; set; } // NAVIGATION Property that tells EF where to find Parent Model/ Table
+
+    }
+}
